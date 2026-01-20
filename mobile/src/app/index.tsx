527482c423bd793e34c0b285e24829132e9480c7
eyshilaburiti <buritieyshila@gmail.com>
2026-01-19 22:52:03 -0300

    feat: setup do projeto

---
+++ mobile/src/app/index.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function Index(){
+
+    return (
+        <View style={styles.container}>
+            <Text>Index</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
